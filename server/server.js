50c19704a08f5efa049f7d48aa388605a43a7bf1
nazuk <nachunazuk@gmail.com>
2026-03-05 17:44:48 +0530

    fix: set CORS origin to hexaweld-ecommerce.vercel.app (was wrongly set to localhost)

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5000/",
+    origin: "https://hexaweld-ecommerce.vercel.app",
     credentials: true
 }));
 
